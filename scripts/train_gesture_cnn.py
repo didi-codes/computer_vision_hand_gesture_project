36f35012211f85a026c5261a14d1b74e96dfecea
Damaris Garcia <63378127+didi-codes@users.noreply.github.com>
2025-11-23 13:52:17 -0500

    First iteration of CNN model (#7)

---
+++ scripts/train_gesture_cnn.py
@@ -0,0 +1,84 @@
+import os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+import matplotlib.pyplot as plt
+
+# Setting dataset path
+DATASET_PATH = "/Users/damarisgarcia/Desktop/computer_vision_hand_gesture_project/dataset"
+
+# Setting Image data generator
+train_datagen = ImageDataGenerator(
+    rescale=1.0/255,
+    validation_split=0.2,
+    rotation_range=20,
+    zoom_range=0.2,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True
+)
+
+train_generator = train_datagen.flow_from_directory(
+    DATASET_PATH,
+    target_size=(64,64),
+    batch_size=32,
+    class_mode="categorical",
+    subset="training"
+)
+
+validation_generator = train_datagen.flow_from_directory(
+    DATASET_PATH,
+    target_size=(64.64),
+    batch_size=32,
+    class_mode="categorical",
+    subset="validation"
+)
+
+# Defining the model
+model = Sequential([
+    Conv2D(32, (3,3), activation="relu", input_shape=(64,64,3)),
+    MaxPooling2D(2,2),
+
+    Conv2D(64, (3,3), activation="relu"),
+    MaxPooling2D(2,2),
+
+    Conv2D(128, (3,3), activation="relu"),
+    MaxPooling2D(2,2),
+
+    Flatten(),
+    Dense(128, activation="relu"),
+    Dropout(0.3),
+    Dense(train_generator.num_classes, activation="softmax")
+])
+
+model.compile(
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.summary()
+
+# Training the model
+history = model.fit(
+    train_generator,
+    validation_data=validation_generator,
+    epochs=15
+)
+
+# Saving the training model
+MODEL_PATH = "/Users/damarisgarcia/Desktop/computer_vision_hand_gesture_project/model/gesture_cnn.h5"
+os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+model.save(MODEL_PATH)
+print(f"Model saved to {MODEL_PATH}")
+
+# Plotting accuracy
+plt.figure(figsize=(8,5))
+plt.plot(history.history["accuracy"], label="Training Accuracy")
+plt.plot(history.history["val_accuracy"], label="Validation Accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.title("Training vs Validation Accuracy")
+plt.legend()
+plt.grid(True)
